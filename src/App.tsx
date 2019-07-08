506b9e958d0b585b68f8a064ef85a17ac8a30c7a
jefferson <cardbeatt@gmail.com>
2019-07-08 19:56:23 -0300

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Welcome from "./components/welcome/welcome";
+import Header from "./components/header/header";
 import { Container } from "./App.styles";
 
 const App: React.FC = () => {
   return (
     <Container>
+      <Header />
       <Welcome />
     </Container>
   );
