284cc076fb8e65d0cda216cc89b9576d0a0439a1
jefferson <cardbeatt@gmail.com>
2019-07-08 20:05:34 -0300

    installed firebase/firebaseui for authentication

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
 
 const Header: React.FC = () => {
   return <div>this is header</div>;
 };
 
 export default Header;
+
+// basic firebase auth
