d6c62a6efbf45ff59f594e0d2a3cffcf5dba5d95
jefferson <cardbeatt@gmail.com>
2019-07-08 20:27:28 -0300

    created the github auth component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase";
+import Keys from "../../Keys";
+
+const config = {
+  apiKey: Keys.firebase,
+  authDomain: "project-140434701386.firebaseapp.com"
+};
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/signedIn",
+  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID]
+};
 
 const Header: React.FC = () => {
-  return <div>this is header</div>;
+  return (
+    <div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
 };
 
 export default Header;
-
-// basic firebase auth
