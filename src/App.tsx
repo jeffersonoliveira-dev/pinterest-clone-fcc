9d383c7caa7395884d4f4bd9c443afc82c0c9c66
jefferson <cardbeatt@gmail.com>
2019-07-10 19:29:24 -0300

    redirecting component to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import Welcome from "./components/welcome/welcome";
 import Header from "./components/header/header";
 import { Container } from "./App.styles";
+import AppRouter from "./routes";
 
 const App: React.FC = () => {
-  return (
-    <Container>
-      <Header />
-      <Welcome />
-    </Container>
-  );
+  return <AppRouter />;
 };
 
 export default App;
