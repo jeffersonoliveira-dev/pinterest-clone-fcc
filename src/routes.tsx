11dbcbfbacbc0d6636ca3c7821b014303b9cd834
jefferson <cardbeatt@gmail.com>
2019-07-10 19:29:41 -0300

    routes controller created

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,1 +1,15 @@
-// exporting routes here with react dom and must render on index.tsx
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from "./components/header/header";
+import Welcome from "./pages/Welcome";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Header />
+      <Route path="/" exact component={Welcome} />
+    </Router>
+  );
+}
+
+export default AppRouter;
