f6220ab7eac500df505b7b4da8cb6db8a8a0f734
jefferson <cardbeatt@gmail.com>
2019-07-24 17:38:30 -0300

    added react redux and created redux folder

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import firebase from "firebase";
 
 const Dashboard: React.FC = () => {
+  console.log(firebase.auth().currentUser);
   return <p> this is the dashboard</p>;
 };
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Link } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/Dashboard";
