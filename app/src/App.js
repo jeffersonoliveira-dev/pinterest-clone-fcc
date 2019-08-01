2e35d1cebc8d397a82e0e2741643a5e87709274b
jefferson <cardbeatt@gmail.com>
2019-08-01 12:21:50 -0300

    adding store/redux

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./redux/store/store";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
