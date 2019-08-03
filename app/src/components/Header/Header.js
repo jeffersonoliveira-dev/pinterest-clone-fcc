0d034125a704cbf0139a6487b01522351d835873
jefferson oliveira <cardbeatt@gmail.com>
2019-08-02 23:35:31 -0300

    firebase ui replacement react component

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -3,7 +3,11 @@ import React, { Fragment } from "react";
 const Header = () => {
   return (
     <Fragment>
-      <nav></nav>
+      <nav>
+	<ul>
+	  <li>firebaseuicomponent</li>
+	</ul>
+      </nav>
     </Fragment>
   );
 };
