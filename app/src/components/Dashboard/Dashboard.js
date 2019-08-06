50d1901e114d5154ea1c5a2057b3607c79275235
jefferson <cardbeatt@gmail.com>
2019-08-06 13:41:01 -0300

    notes

--- app/src/components/Dashboard/Dashboard.js
+++ app/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// using masonry,js to display image like pinterest
+
 const Dashboard = () => {
   return <>this is the dashboard</>;
 };
--- app/src/components/ImageButton/ImageButton.js
+++ app/src/components/ImageButton/ImageButton.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// upload image and check if isnt broken , otherwise upload a placeholder image
+
 const ImageButton = () => {
   return (
     <>
