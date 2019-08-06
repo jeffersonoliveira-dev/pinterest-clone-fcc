cddee12c67c5170e99e727f1e72f43fdcf9b7261
jefferson <cardbeatt@gmail.com>
2019-08-05 23:49:21 -0300

    firebase config

---
+++ app/src/firebase.js
@@ -0,0 +1,8 @@
+import firebase from "firebase";
+import { Config } from "./Keys";
+import("firebase/firestore");
+
+firebase.initializeApp(Config);
+const database = firebase.firestore();
+
+export default database;
