c2faebc6de365f44625db9bdf163b0f0c6b262dc
jefferson <cardbeatt@gmail.com>
2019-08-06 16:42:17 -0300

    delete home to just renderize pictures even if the member isnt authenticated

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Image from "./components/Image/Image";
 
@@ -12,7 +11,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Header />
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Dashboard} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/image" component={Image} />
       </Router>
