09ac0059b2c2f018223a31bbb56a24ff7499c231
jefferson <cardbeatt@gmail.com>
2019-08-07 14:06:05 -0300

    added github link to use later

--- app/src/components/Dashboard/Dashboard.js
+++ app/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 // using masonry,js to display image like pinterest
+// https://github.com/tsuyoshiwada/react-stack-grid
 
 const Dashboard = () => {
   return <>this is the dashboard</>;
