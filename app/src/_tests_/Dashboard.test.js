a01d1545958ef126aa95258eec9c2e5827947fa4
jefferson <cardbeatt@gmail.com>
2019-08-08 18:43:34 -0300

    more tests!

--- app/src/_tests_/Dashboard.test.js
+++ app/src/_tests_/Dashboard.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Dashboard from "../components/Dashboard/index";
 afterEach(cleanup);
 
-test("render component ", () => {
+it("render component ", () => {
   const { asFragment } = render(<Dashboard />);
+
   expect(asFragment()).toMatchSnapshot();
+});
+
+it("text render", () => {
+  const { getByText } = render(<Dashboard text="this is the dashboard" />);
+
+  expect(getByText("this is the dashboard")).not.toHaveClass("page");
 });
