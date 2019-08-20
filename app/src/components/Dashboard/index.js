b65a4d4a30efb3798cf799183bf0afcb8021135b
jefferson oliveira <cardbeatt@gmail.com>
2019-08-20 19:49:08 -0300

    fetching images from firebase firestore

--- app/src/components/Dashboard/index.js
+++ app/src/components/Dashboard/index.js
@@ -1,12 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React, {useEffect} from 'react';
+import styled from 'styled-components';
+import database from '../../firebase';
 
 const Container = styled.div`
   display: flex;
 `;
 
-const Dashboard = ({ text }) => {
-  return <Container>{text}</Container>;
+const Dashboard = () => {
+  useEffect(() => {
+    // firebase code here
+    database
+      .collection('users')
+      .get()
+      .then(snapshot => {
+        snapshot.docs.map(doc => {
+          console.log(doc.data());
+          // display image
+        });
+      });
+  }, []);
+  return <Container>hello</Container>;
 };
 
 export default Dashboard;
