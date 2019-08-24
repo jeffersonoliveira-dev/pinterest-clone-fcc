4e34280569e61477ac03a25a33eb10c9d345280b
jefferson oliveira <cardbeatt@gmail.com>
2019-08-24 19:28:39 -0300

    just testing

--- app/src/components/Profile/index.js
+++ app/src/components/Profile/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import database from '../../firebase';
 
-
 export default function Profile({match}) {
-  // maybe useEffect 
+  // maybe useEffect
   // match.params.id => token
   // get firebase
   return <div>this is profile </div>;
