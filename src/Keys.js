63d5718a0cab528bdd052c3ea1bb26b894754630
jefferson oliveira <cardbeatt@gmail.com>
2019-08-27 16:10:12 -0300

    pushing from source

---
+++ src/Keys.js
@@ -0,0 +1,9 @@
+export const Config = {
+  apiKey: 'AIzaSyDr8qkrKiRDMtV9VCqIM0xeh6ZRLPW898o',
+  authDomain: 'pinclonerest.firebaseapp.com',
+  databaseURL: 'https://pinclonerest.firebaseio.com',
+  projectId: 'pinclonerest',
+  storageBucket: 'pinclonerest.appspot.com',
+  messagingSenderId: '140434701386',
+  appId: '1:140434701386:web:2a9f8fa32221d232',
+};
